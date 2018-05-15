068d06addc97772450e722d744419b32b1649f1f
Cam Holloway <hollowprince@outlook.com>
2018-05-15 17:05:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 // write your code below!
-var 
 function happyHolidays() {
   return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+  return "Happy holidays, ${name}!"
+}
+
+function happyHolidayTo(holiday, name) {
+  return "Happy ${holiday}, ${name}!"
+}
+
+function holidayCountdown(days, holiday) {
+  return "Happy ${holiday}, ${name}"
 }
